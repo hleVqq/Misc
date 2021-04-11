bb904a06af677a1fa3b01309803095c3fb6c34c7
hleV <hlevqq@gmail.com>
2021-04-11 22:01:33 +0300

    Add files via upload

--- eac3-to-aac.cmd
+++ eac3-to-aac.cmd
@@ -10,7 +10,9 @@ for %%i in (%input%) do (
 )
 
 set output=%fullPath%%file%.aac%ext%
-ffmpeg -i %input% -vcodec copy -acodec aac -ab 320K -strict -2 %output%
+@REM ffmpeg -i %input% -map 0:s -c copy -vcodec copy -acodec aac -ab 320K %output%
+@REM ffmpeg -i %input% -map 0:v -map 0:a -c aac -map 0:s -c copy %output%
+ffmpeg -i %input% -map 0 -c copy -c:a aac -ab 320K %output%
 echo Saved to %output%
 
 pause
